374bc9c184e1c01c7e50903ee450bec99e76d325
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-24 12:46:13 +0530

    [Riya] Add : PrintMax Generic Method

--- FindMaximum/GenericsMethod.cs
+++ FindMaximum/GenericsMethod.cs
@@ -13,18 +13,25 @@ namespace FindMaximum
             Comparer<T> comparer = Comparer<T>.Default;
             if (comparer.Compare(a, b) >= 0 && comparer.Compare(a, c) >= 0)
             {
-                Console.WriteLine(a + " is max");
+                T max = a;
+                PrintMax(max);
             }
 
             if (comparer.Compare(b, a) >= 0 && comparer.Compare(b, c) >= 0)
             {
-                Console.WriteLine(b + " is max");
+                T max = b;
+                PrintMax(max);
             }
 
             if (comparer.Compare(c, a) >= 0 && comparer.Compare(c, b) >= 0)
-            {
-                Console.WriteLine(c + " is max");
+            { 
+                T max = c;
+                PrintMax(max);
             }
+        }
+        public void PrintMax<T>(T max)
+        {
+            Console.WriteLine("Maximum value: " + max);
         }
     }
 }
